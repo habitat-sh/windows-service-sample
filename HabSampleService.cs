d009969020053d8fd7498c55bddc6b82130a7a53
mwrock <matt@mattwrock.com>
2017-12-20 16:37:42 -0800

    initial commit
    Signed-off-by: mwrock <matt@mattwrock.com>

---
+++ HabSampleService.cs
@@ -0,0 +1,66 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace HabitatSample
+{
+    public partial class HabSampleService : ServiceBase
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new HabSampleService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        public HabSampleService()
+        {
+            ServiceName = "HabSampleService";
+            CanStop = true;
+            AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteStatus("HabSampleService is starting");
+            System.Timers.Timer timer = new System.Timers.Timer
+            {
+                Interval = 60000 // 60 seconds  
+            };
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+            WriteStatus("HabSampleService is started");
+        }
+
+        protected override void OnStop()
+        {
+            WriteStatus("HabSampleService is stopped");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            WriteStatus("HabSampleService is up");
+        }
+
+        private static void WriteStatus(string message)
+        {
+            var statusFileDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (ConfigurationManager.AppSettings["statusFileDir"] != null)
+            {
+                statusFileDir = ConfigurationManager.AppSettings["statusFileDir"];
+            }
+            using (FileStream fs = new FileStream(Path.Combine(statusFileDir, "status.txt"), FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+    }
+}
---
+++ habitat/plan.ps1
@@ -0,0 +1,23 @@
+﻿$pkg_name="windows-service-sample"
+$pkg_origin="mwrock"
+$pkg_version="0.1.0"
+$pkg_source="nosuchfile.tar.gz"
+$pkg_maintainer="Matt Wrock"
+$pkg_license=@('MIT')
+$pkg_description="A sample .NET Windows Service"
+$pkg_bin_dirs=@("bin")
+
+function invoke-download { }
+function invoke-verify { }
+
+function Invoke-Build {
+  Copy-Item $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force -Exclude ".vs"
+  ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" $HAB_CACHE_SRC_PATH/$pkg_dirname/${pkg_name}.csproj /t:Build /p:Configuration=Release
+  if($LASTEXITCODE -ne 0) {
+      Write-Error "dotnet build failed!"
+  }
+}
+
+function Invoke-Install {
+  Copy-Item $HAB_CACHE_SRC_PATH/$pkg_dirname/bin/release/* $pkg_prefix/bin
+}
