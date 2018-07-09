033f566580d82292944407d1846cb456a822d4d0
mwrock <matt@mattwrock.com>
2018-07-09 11:33:41 -0700

    moderniz plan
    Signed-off-by: mwrock <matt@mattwrock.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -6,9 +6,6 @@ $pkg_license=@('MIT')
 $pkg_description="A sample .NET Windows Service"
 $pkg_bin_dirs=@("bin")
 
-function invoke-download { }
-function invoke-verify { }
-
 function Invoke-Build {
   Copy-Item $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force -Exclude ".vs"
   ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" $HAB_CACHE_SRC_PATH/$pkg_dirname/${pkg_name}.csproj /t:Build /p:Configuration=Release
